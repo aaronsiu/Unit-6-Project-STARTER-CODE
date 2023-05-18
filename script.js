0afb3988fcb7a5ca668611baadc176679cd25803
aaronsiu <123706983+aaronsiu@users.noreply.github.com>
2023-05-18 16:35:57 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+// Declare your genre arrays here
+
+// Make sure to declare your HTML elements as variables! 
+
+// Submit Button 
+let submitButton = document.querySelector("button"); 
+submitButton.onclick = function() {
+    
+}; 
+
+
